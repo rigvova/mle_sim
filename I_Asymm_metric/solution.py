d165fc63e2b1e66b0faf9a03b2918de63dce83cc
vladimir-rigvava <rigvava-vladimir@yandex.ru>
2023-05-18 16:22:48 +0400

    cosmetic changes

--- I_Asymm_metric/solution.py
+++ I_Asymm_metric/solution.py
@@ -2,8 +2,10 @@ import numpy as np
 
 def turnover_error(y_true: np.array, y_pred: np.array) -> float:
     """Returns asymmetric loss that punishes for missed profit."""
+    diff = y_true - y_pred
+    demominator = np.abs(y_pred) + 1  # we need to add 1 in denominator for cases when y_pred==0
     loss = np.sum(
-        ((y_true - y_pred) / (np.abs(y_pred)+1)) ** 2
+        (diff / demominator) ** 2  
     )
 
     return loss
